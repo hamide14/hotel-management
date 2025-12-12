2f0981420a5b88d1212a359fd2b4b81eebaf947e
Your Name <you@example.com>
2025-12-12 13:41:56 +0330

    changed 4 files

--- hotel_management/accounts/forms.py
+++ hotel_management/accounts/forms.py
@@ -1,18 +1,14 @@
-
-from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-
 from django import forms
 
 class SignInForm(forms.Form):
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
-    phone_number = forms.CharField(max_length=15)
-    email = forms.EmailField()
-    password1 = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
+    first_name = forms.CharField(max_length=50, label="First Name")
+    last_name = forms.CharField(max_length=50, label="Last Name")
+    phone_number = forms.CharField(max_length=15, label="Phone Number")
+    email = forms.EmailField(label="Email")
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Password")
+    password2 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
 
 class LogInForm(forms.Form):
-    phone_number = forms.CharField(max_length=15)
-    password = forms.CharField(widget=forms.PasswordInput)
+    phone_number = forms.CharField(max_length=15, label="Phone Number")
+    password = forms.CharField(widget=forms.PasswordInput, label="Password")
--- hotel_management/accounts/views.py
+++ hotel_management/accounts/views.py
@@ -1,15 +1,11 @@
-from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.shortcuts import render
 from .forms import SignInForm, LogInForm
-
 
 def SignIn(request):
     form = SignInForm()
     return render(request, 'signin/signin.html', {'form': form})
 
 
-
 def LogIn(request):
-    pass
+    form = LogInForm()
+    return render(request, 'login/login.html', {'form': form})
