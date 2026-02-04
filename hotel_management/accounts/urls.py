5832593e5837d27788af7add4398d988375a7ddc
Your Name <you@example.com>
2026-02-04 09:43:40 +0330

    modified urls.py and views.py in accounts

--- hotel_management/accounts/urls.py
+++ hotel_management/accounts/urls.py
@@ -1,18 +1,34 @@
+# from django.urls import path
+# from .views import signup, signup_done, login_view
+# from django.contrib.auth.views import LogoutView
+# from django.urls import path
+# from . import views
+
+# app_name = "accounts"
+
+# urlpatterns = [
+#     path("signup/", signup, name="signup"),
+#     path("signup_done/", signup_done, name="signup_done"),
+#     path("login/", login_view, name="login"),
+#     path("logout/", LogoutView.as_view(next_page='/home/'), name="logout"),
+#     path("otp/", views.send_otp, name="send_otp"),
+#     path("otp/verify/", views.verify_otp, name="verify_otp"),
+    
+
+# ]
+
+
 from django.urls import path
-from .views import signup, signup_done, login_view
 from django.contrib.auth.views import LogoutView
-from django.urls import path
 from . import views
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("signup/", signup, name="signup"),
-    path("signup_done/", signup_done, name="signup_done"),
-    path("login/", login_view, name="login"),
+    path("signup/", views.signup, name="signup"),
+    # path("signup_done/", views.signup_done, name="signup_done"),  # دیگه لازم نیست
+    path("login/", views.login_view, name="login"),
     path("logout/", LogoutView.as_view(next_page='/home/'), name="logout"),
-    path("otp/", views.send_otp, name="send_otp"),
-    path("otp/verify/", views.verify_otp, name="verify_otp"),
-    
-
+    path("otp/", views.send_otp, name="send_otp"),           
+    path("otp/verify/", views.verify_otp, name="verify_otp"), 
 ]
--- hotel_management/accounts/views.py
+++ hotel_management/accounts/views.py
@@ -3,13 +3,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from .forms import CustomUserCreationForm, LoginForm
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-import random
-from django.conf import settings
-from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.contrib.auth import get_user_model
+import random
+from django.conf import settings
 from .models import EmailOTP
 from .forms import EmailForm, OTPForm
 
@@ -20,18 +17,28 @@ def signup(request):
         form = CustomUserCreationForm(request.POST)
         # we validate data 
         if form.is_valid():
-            form.save() #save in customeruser model and go to done page
-            return redirect("accounts:signup_done")
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+
+            code = str(random.randint(100000, 999999))
+            EmailOTP.objects.create(user=user, code=code)
+
+            send_mail(
+                "Your Signup OTP",
+                f"Your OTP code is: {code}",
+                settings.DEFAULT_FROM_EMAIL,
+                [user.email],
+                fail_silently=False
+            )
+
+            request.session["otp_user_id"] = user.id
+            return redirect("accounts:verify_otp")
     else:# its Get so empty form there
         form = CustomUserCreationForm()
         
     #context render and form goes to context
     return render(request, "signup/signup.html", {"form": form})
-
-
-def signup_done(request):
-    return render(request, "signup/signup_done.html")
-
 
 
 def login_view(request):
@@ -49,18 +56,18 @@ def login_view(request):
             user = authenticate(request, username=phone, password=password)
             #authenticate return user object or none
             if user:
-                login(request, user)
-                return redirect("home:home")
+                if user.is_active:
+                    login(request, user)
+                    return redirect("home:home")
+                else:
+                    request.session["otp_user_id"] = user.id
+                    return redirect("accounts:verify_otp")
             else:
-                
                 messages.error(request, "Phone number or password is incorrect")
-                #render form again for try again
                 return render(request, "login/login.html", {"form": form})
     else: #if its get just empty form 
         form = LoginForm()
         
-    # render template (create html page and send to browser)
-    # we got url of template and we send our forms to it also errors
     return render(request, "login/login.html", {"form": form})
 
 
@@ -73,39 +80,32 @@ def send_otp(request):
         if form.is_valid():
             email = form.cleaned_data["email"]
             
-            # do we have user with this email?
-            user = User.objects.filter(email=email).first()# no? none
-            #if not show the send otp form agina and user not found
+            user = User.objects.filter(email=email).first()
             if not user:
                 return render(request, "OTP/send_otp.html", {
                     "form": form,
                     "error": "User not found"
                 })
 
-            #delete old otps for this user 
             EmailOTP.objects.filter(user=user).delete()
             
-            #random 6 digite code
-
             code = str(random.randint(100000, 999999))
-            
             EmailOTP.objects.create(user=user, code=code)
-            # function for sending email
+
             send_mail(
-                "Your Login Code",                # subject
-    f"Your OTP code is: {code}",     # message
-    settings.DEFAULT_FROM_EMAIL,      # from_email
-    [email],                          # recipient_list
-    fail_silently=False
+                "Your Login Code",
+                f"Your OTP code is: {code}",
+                settings.DEFAULT_FROM_EMAIL,
+                [email],
+                fail_silently=False
             )
 
-            request.session["otp_user_id"] = user.id #store user id in session for later use
+            request.session["otp_user_id"] = user.id
             return redirect("accounts:verify_otp")
     else:#its a Get (empy form for email)
         form = EmailForm()
 
     return render(request, "OTP/send_otp.html", {"form": form})
-
 
 
 def verify_otp(request):
@@ -123,8 +123,11 @@ def verify_otp(request):
                 code=code
             ).order_by("-created_at").first()
 
-            if otp and otp.is_valid() and otp.user.is_active:
-                login(request, otp.user)
+            if otp and otp.is_valid():
+                user = otp.user
+                user.is_active = True
+                user.save()
+                login(request, user)
                 otp.delete()
                 request.session.pop("otp_user_id", None)
                 return redirect("home:home")
@@ -134,39 +137,3 @@ def verify_otp(request):
         form = OTPForm()
 
     return render(request, "OTP/verify_otp.html", {"form": form})
-
-
-
-# def verify_signup_otp(request):
-#     user_id = request.session.get("otp_user_id")
-#     if not user_id:
-#         return redirect("accounts:signup")
-
-#     user = get_object_or_404(User, id=user_id)
-
-#     if request.method == "POST":
-#         form = OTPForm(request.POST)
-#         if form.is_valid():
-#             code = form.cleaned_data["code"]
-
-#             otp = EmailOTP.objects.filter(
-#                 user=user,
-#                 code=code
-#             ).order_by("-created_at").first()
-
-#             if otp and otp.is_valid():
-#                 user.is_active = True
-#                 user.save()
-
-#                 otp.delete()
-#                 request.session.pop("otp_user_id", None)
-
-#                 login(request, user)
-#                 return redirect("home:home")
-#             else:
-#                 messages.error(request, "Invalid or expired OTP")
-#     else:
-#         form = OTPForm()
-
-#     return render(request, "OTP/verify_otp.html", {"form": form})
-
